ab400e9402aa936e5af67850fe343fce14284e4d
iliasooox <igoutilias@gmail.com>
2026-02-02 23:29:54 +0100

    Add prayer times Android Compose UI

---
+++ app/src/main/java/com/example/prayertime/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.example.prayertime.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val NightSky = Color(0xFF0A1D37)
+val SunriseGold = Color(0xFFFFC857)
+val SoftBlue = Color(0xFF9DB4D1)
---
+++ app/src/main/java/com/example/prayertime/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.example.prayertime.ui.theme
+
+import androidx.compose.material3.Typography
+
+val Typography = Typography()
